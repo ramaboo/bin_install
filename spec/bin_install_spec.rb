5180a5aa26103cddb3d846667147803424a98eb1
David Singer <david@ramaboo.com>
2018-05-16 17:40:03 -0700

    test

--- spec/bin_install_spec.rb
+++ spec/bin_install_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe BinInstall do
   it 'has a version number' do
     expect(BinInstall::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
