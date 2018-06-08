6d907afc599162eea25cff4cfadc4d3ed4b135a2
David Singer <david@ramaboo.com>
2018-06-07 20:18:27 -0700

    update

--- lib/bin_install/oh_my_zsh.rb
+++ lib/bin_install/oh_my_zsh.rb
@@ -6,15 +6,15 @@ module BinInstall
 
     def self.install
       puts 'Installing Oh My Zsh...'.white
-      Brew::Package.install('zsh')
-      Brew::Package.install('zsh-completions')
+      Brew::Package.install_or_upgrade('zsh')
+      Brew::Package.install_or_upgrade('zsh-completions')
       system(%(sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"))
     end
 
     def self.install!
       puts 'Installing Oh My Zsh...'.white
-      Brew::Package.install!('zsh')
-      Brew::Package.install!('zsh-completions')
+      Brew::Package.install_or_upgrade!('zsh')
+      Brew::Package.install_or_upgrade!('zsh-completions')
       BinInstall.system!(%(sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"))
     end
 
