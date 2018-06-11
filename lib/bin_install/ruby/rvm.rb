6ebbd1d1048292775e8712e90892843e317fc539
David Singer <david@ramaboo.com>
2018-06-11 15:05:06 -0700

    Releasing 0.0.26

--- lib/bin_install/ruby/rvm.rb
+++ lib/bin_install/ruby/rvm.rb
@@ -15,23 +15,31 @@ module BinInstall
         install_ruby!
       end
 
-      def self.install_ruby(version = nil)
-        version ||= Ruby.required_ruby_version
+      def self.install_ruby(version = Ruby.required_ruby_version)
+        puts "Installing Ruby #{version}...".white
 
         if version
-          system("rvm install #{version}")
-          system("rvm use #{version}")
+          if Ruby.ruby_version_installed?(version)
+            puts "Ruby #{version} is already installed. Skipping Ruby #{version} install.".blue
+          else
+            system("rvm install #{version}")
+            system("rvm use #{version}")
+          end
         else
           puts 'Unknown Ruby version. Create .ruby-version file.'
         end
       end
 
-      def self.install_ruby!(version = nil)
-        version ||= Ruby.required_ruby_version
+      def self.install_ruby!(version = Ruby.required_ruby_version)
+        puts "Installing Ruby #{version}...".white
 
         if version
-          BinInstall.system!("rvm install #{version}")
-          BinInstall.system!("rvm use #{version}")
+          if Ruby.ruby_version_installed?(version)
+            puts "Ruby #{version} is already installed. Skipping Ruby #{version} install.".blue
+          else
+            BinInstall.system!("rvm install #{version}")
+            BinInstall.system!("rvm use #{version}")
+          end
         else
           abort('Unknown Ruby version. Create .ruby-version file.'.red)
         end
--- lib/bin_install/version.rb
+++ lib/bin_install/version.rb
@@ -1,3 +1,3 @@
 module BinInstall
-  VERSION = '0.0.25'.freeze
+  VERSION = '0.0.26'.freeze
 end
